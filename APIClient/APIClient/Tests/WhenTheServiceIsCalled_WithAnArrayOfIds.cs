97152563d34204214e58fde32cd33104b2c81347
Lenzed26 <denzeklfc99@gmail.com>
2021-08-20 13:27:52 +0100

    Added skeleton code for BulkCharacters

---
+++ APIClient/APIClient/Tests/WhenTheServiceIsCalled_WithAnArrayOfIds.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace APIClient.Tests
+{
+    class WhenTheServiceIsCalled_WithAnArrayOfIds
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+
+        }
+
+        [Test]
+        [Category("Happy")]
+        public void StatusCodeIs200()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        [Category("Happy")]
+        public void ResponseIsNotNull()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        [Category("Happy")]
+        public void ResponseId1_NameIsRickSanchez()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        [Category("Happy")]
+        public void Response_ReturnsCorrectResult()
+        {
+            Assert.Fail();
+        }
+    }
+}
