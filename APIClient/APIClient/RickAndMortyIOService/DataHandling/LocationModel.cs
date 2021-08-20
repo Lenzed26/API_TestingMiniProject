7c32ccb6dcc3950b3bac3fdd5017fee9a96edc83
jkarlokc <jkarlokc@gmail.com>
2021-08-20 14:55:07 +0100

    Created Location Model and List Of Locations Service Class

---
+++ APIClient/APIClient/RickAndMortyIOService/DataHandling/LocationModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIClient.RickAndMortyIOService.DataHandling
+{
+
+    public class LocationResponse : IResponse
+    {
+        public Info info { get; set; }
+        public LocationDetails[] LocationDetails { get; set; }
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public object prev { get; set; }
+    }
+
+    public class LocationDetails
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string dimension { get; set; }
+        public string[] residents { get; set; }
+        public string url { get; set; }
+        public DateTime created { get; set; }
+    }
+
+}
---
+++ APIClient/APIClient/RickAndMortyIOService/Service/ListOfLocationsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using APIClient.RickAndMortyIOService.DataHandling;
+using APIClient.RickAndMortyIOService.HTTPManager;
+
+namespace APIClient.RickAndMortyIOService.Service
+{
+    class ListOfLocationsService
+    {
+    }
+}
