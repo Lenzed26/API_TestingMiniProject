d25cd72ef49f50ad6841419ecdb1acb3a7df7717
umz-eats-doner-kebab <Umar.T.Majid@gmail.com>
2021-08-20 14:35:31 +0100

    Created Seperate test file to test for single character and single id

--- APIClient/APIClient/Tests/WhenTheSingleCharacterServiceIsCalled_WithOneID.cs
+++ APIClient/APIClient/Tests/WhenTheSingleCharacterServiceIsCalled_WithOneID.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace APIClient.RickAndMortyIOService.Service
+namespace APIClient.Tests
 {
-    class SingleEpisodeService
+    class WhenTheSingleCharacterServiceIsCalled_WithOneID
     {
-
     }
 }
