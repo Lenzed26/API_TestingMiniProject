8df40d87c103c581b7e870bc0138fdd6c687bdbe
Lenzed26 <denzeklfc99@gmail.com>
2021-08-20 12:21:31 +0100

    Created skeleton code for framework

---
+++ APIClient/APITestApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("APITestApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("APITestApp")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c15d51d-170e-4bad-a1bb-128f890fcf8e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ APIClient/APITestApp/RickAndMortyIOService/DataHandling/DTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APITestApp.RickAndMortyIOService.DataHandling
+{
+    public class DTO<ResponseType> where ResponseType : IResponse, new()
+    {
+    }
+}
