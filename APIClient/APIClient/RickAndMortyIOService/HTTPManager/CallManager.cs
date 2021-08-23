eebf33d7bc0a230de9d4258628ba85132a73d97c
umz-eats-doner-kebab <Umar.T.Majid@gmail.com>
2021-08-23 13:33:20 +0100

    Refactored the CallManager Class

--- APIClient/APIClient/RickAndMortyIOService/HTTPManager/CallManager.cs
+++ APIClient/APIClient/RickAndMortyIOService/HTTPManager/CallManager.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json.Linq;
+﻿using System.Threading.Tasks;
 using RestSharp;
 
 namespace APIClient.RickAndMortyIOService.HTTPManager
@@ -11,10 +6,13 @@ namespace APIClient.RickAndMortyIOService.HTTPManager
     public class CallManager
     {
         private readonly IRestClient _client;
+        private readonly RestRequest request;
         public int StatusCode { get; set; }
 
         public CallManager()
         {
+            request = new RestRequest();
+            request.AddHeader("Content-Type", "application/json");
             _client = new RestClient(AppConfigReader.BaseUrl);
         }
 
@@ -22,10 +20,7 @@ namespace APIClient.RickAndMortyIOService.HTTPManager
         {
             var request = new RestRequest(Method.GET);
 
-            request.AddHeader("Content-Type", "application/json");
-
             request.Resource = $"episode/{episode.ToLower().Replace(" ", "")}";
-
             var response = await _client.ExecuteAsync(request);
 
             StatusCode = (int)response.StatusCode;
@@ -35,8 +30,6 @@ namespace APIClient.RickAndMortyIOService.HTTPManager
         public async Task<string> MakeBulkEpisodeRequestAsync()
         {
             var request = new RestRequest(Method.GET);
-
-            request.AddHeader("Content-Type", "application/json");
 
             request.Resource = $"episode";
 
@@ -48,45 +41,29 @@ namespace APIClient.RickAndMortyIOService.HTTPManager
 
         public async Task<string> MakeBulkCharacterRequestAsync()
         {            
-            var request = new RestRequest();
-            request.AddHeader("Content-Type", "application/json");
-
             request.Resource = $"character";
-
             var response = await _client.ExecuteAsync(request);        
-            
-
             StatusCode = (int)response.StatusCode;
             return response.Content;
         }
 
         public async Task<IRestResponse> MakeSingleLocationRequestAsync(int id)
         {
-            //Set up the Request
-            var request = new RestRequest();
-            request.AddHeader("Content-Type", "application/json");
+
             //Define request resource path
             request.Resource = $"location/{id}";
             //Make Request and Return the Response 
             return await _client.ExecuteAsync(request);
-            
-
         }
 
 
         public async Task<string> MakeSingleCharacterRequestAsync(int id)
         {
-            var request = new RestRequest();
-            request.AddHeader("Content-Type", "application/json");
-
             request.Resource = $"character/{id}";
-
             var response = await _client.ExecuteAsync(request);
-
             StatusCode = (int)response.StatusCode;
             return response.Content;
         }
-
 
     }
 }
